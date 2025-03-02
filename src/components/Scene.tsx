84d45172a94ccabbc6d8a399c2961246358210f7
Jye J. Stillitano <jyestillitano@hotmail.com>
2025-03-02 17:40:26 +1100

    Simple animations added. Cursor updated. Packages saved exact.

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,28 +1,44 @@
 "use client"
 
+// React Three Fiber Imports
 import * as THREE from "three"
-
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useLoader } from "@react-three/fiber"
 import { Text } from "@react-three/drei"
-
+import {
+	EffectComposer,
+	Outline,
+	DotScreen,
+	Pixelation,
+	Scanline,
+} from "@react-three/postprocessing"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+// Import performance monitor.
+import { Perf } from "r3f-perf"
 
 import { easing } from "maath"
 
+import { clamp } from "../util/Clamp"
+import { useRef, useState } from "react"
+
 // Easing Camera - Look At Mouse
 function Camera() {
+	const defaultPosition: THREE.Vector3 = new THREE.Vector3(0, 10, 12)
+	const closeupPosition: THREE.Vector3 = new THREE.Vector3(0, 5, 6)
+	const currentPosition: THREE.Vector3 = defaultPosition
 	const targetPosition: THREE.Vector3 = new THREE.Vector3(0, 0, 0)
 
 	useFrame((state, delta) => {
 		targetPosition.set(
-			1 + (state.pointer.x * state.viewport.width) / 3,
-			(20 + state.pointer.y) / 2,
-			15,
+			currentPosition.x +
+				clamp((state.pointer.x * state.viewport.width) / 3, -10, 10),
+			currentPosition.y + state.pointer.y,
+			currentPosition.z,
 		)
 
 		easing.damp3(state.camera.position, targetPosition, 0.5, delta)
-		state.camera.lookAt(0, 3, 0)
+		state.camera.lookAt(0, 5, 0)
 	})
 	return <></>
 }
@@ -45,9 +61,9 @@ function Lighting() {
 
 	return (
 		<>
-			<ambientLight intensity={0.3} />
+			<ambientLight intensity={1} />
 			<pointLight
-				position={[3, 5, 3]}
+				position={[2, 5, 3]}
 				intensity={40}
 				distance={20}
 				decay={2}
@@ -60,6 +76,8 @@ function Lighting() {
 }
 
 function Gameboy() {
+	const [hovered, setHovered] = useState(false)
+	const ref = useRef<THREE.Mesh | null>(null)
 	// Load gameboy model.
 	const model = useLoader(GLTFLoader, "/gameboy/scene.gltf")
 
@@ -71,15 +89,27 @@ function Gameboy() {
 		}
 	})
 
+	useFrame((_, delta) => {
+		if (ref.current) {
+			if (hovered)
+				easing.dampE(ref.current?.rotation, [0, Math.PI * 4, 0], 0.5, delta)
+			else easing.dampE(ref.current?.rotation, [0, Math.PI, 0], 0.5, delta)
+		}
+	})
+
 	// Scale and return primitive object.
 	return (
 		<mesh
-			position={[0, 3.8, 0]}
+			position={[0.25, 4, 1]}
 			rotation={[0, Math.PI, 0]}
 			scale={[0.5, 0.5, 0.5]}
 			castShadow
+			onPointerEnter={() => setHovered(true)}
+			onPointerLeave={() => setHovered(false)}
+			ref={ref}
 		>
 			<primitive object={model.scene} />
+			<meshBasicMaterial />
 		</mesh>
 	)
 }
@@ -90,7 +120,7 @@ function Information() {
 			<Text
 				color="white"
 				font="/fonts/AtkinsonHyperlegibleMono-VariableFont_wght.ttf"
-				fontSize={2}
+				fontSize={1.5}
 				anchorX="center"
 				position={[0, 10, -2]}
 				rotation={[-0.5, 0, 0]}
@@ -102,43 +132,55 @@ function Information() {
 				font="/fonts/AtkinsonHyperlegibleMono-VariableFont_wght.ttf"
 				fontSize={0.5}
 				anchorX="center"
-				position={[-7.5, 5, 0.75]}
+				position={[-5, 5, 0.75]}
 				rotation={[-0.5, 0, 0]}
 				maxWidth={5}
 			>
-				Now you can have that exciting Nintendo action anywhere with the new
-				GAME BOY video system. Just pop it in your pocket and pull it out
-				anytime.
+				With a dot-matrix screen plus standard controls you'll recognize, the
+				GAME BOY is capable of playing a wide range of games using
+				interchangable cartridges.
 			</Text>
 			<Text
 				color="white"
 				font="/fonts/AtkinsonHyperlegibleMono-VariableFont_wght.ttf"
 				fontSize={0.5}
 				anchorX="center"
-				position={[6.5, 5, 0.75]}
+				position={[6, 5, 0.75]}
 				rotation={[-0.5, 0, 0]}
-				maxWidth={7}
+				maxWidth={5.5}
 			>
-				With a video link that hooks up to another GAME BOY system for 2-player
-				competition, stereo headphones, external speaker, battery and Tetris
-				cartridge, you can take the GAME BOY to the beach, park or playground.
+				Including a video link for 2-player competition, stereo headphones,
+				external speaker and battery, you can take the GAME BOY to the beach,
+				park or playground.
 			</Text>
 
 			<Text
 				color="white"
 				font="/fonts/AtkinsonHyperlegibleMono-VariableFont_wght.ttf"
-				fontSize={1.75}
+				fontSize={1.5}
 				anchorX="center"
-				position={[0, 0.1, 3.5]}
+				position={[1, 0.1, 3.5]}
 				rotation={[-Math.PI / 2, 0, 0]}
 			>
-				Compact Game System
+				Wherever. Whenever.
 			</Text>
 		</>
 	)
 }
 
+// Simple model for debugging.
+function Box() {
+	return (
+		<mesh position={[0, 5, 0]}>
+			<boxGeometry args={[2, 2, 2]} />
+			<meshStandardMaterial />
+			{/* <Outlines thickness={10} color="hotpink" /> */}
+		</mesh>
+	)
+}
+
 export default function Scene() {
+	//const [focused, outline] = useState(false)
 	// Camera Controller
 	// https://codesandbox.io/p/sandbox/bst0cy?file=%2Fsrc%2FApp.js%3A40%2C51-40%2C59
 
@@ -149,10 +191,18 @@ export default function Scene() {
 		<Canvas shadows>
 			<Camera />
 			<Lighting />
-			<Gameboy />
+
 			<Information />
 			<Background />
-			{/* <Perf position="top-right" /> */}
+			<EffectComposer autoClear={false}>
+				{/* <Scanline density={2} /> */}
+				{/* <Pixelation granularity={50} /> */}
+				{/* <DotScreen scale={2} /> */}
+				<Outline />
+			</EffectComposer>
+			<Gameboy />
+
+			<Perf position="top-right" />
 		</Canvas>
 	)
 }
